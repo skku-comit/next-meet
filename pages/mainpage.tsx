a94fd8ef3385f9aa6547d6db8f4edfd9fcd80d0d
JungHwan <wjdghksdl00@gmail.com>
2024-01-24 15:08:38 +0900

    first commit

---
+++ pages/mainpage.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+
+const MainPage = ():ReactNode =>{
+
+
+    return <div className="w-screen h-full min-h-screen ">
+
+
+    </div>
+}
+
+export default MainPage;
