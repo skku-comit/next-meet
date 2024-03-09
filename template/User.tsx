5c95a94d8ae4f0779e4e2e56596af130829c72e8
toothless8392 <hsp5235@naver.com>
2024-03-09 19:58:05 +0900

    password is not required

--- template/User.tsx
+++ template/User.tsx
@@ -1,12 +1,11 @@
 export interface User {
   userID: number; //unique id for identification
   userName: string;
-  password: string|null;
+  password: string|null;  // Password would be null except for the case of "credenetials"
 }
 
 export interface NextMeetUser extends User {
   provider: 'credentials'|'google';
-  password: string;
   loginID: string;
   email: string;
   eventIDList: string[]; //이미 참여한 이벤트 or 자신이 호스트인 이벤트
