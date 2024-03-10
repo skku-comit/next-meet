8eb0c578b801bcf68fc14d987fa1f9e7b75be631
Hyewonjang <hyewon5254295@gmail.com>
2024-03-11 00:31:40 +0900

    lang btn edit

--- components/LangButton.tsx
+++ components/LangButton.tsx
@@ -12,11 +12,11 @@ const LangButton = ({ lang, setLang }: LangButtonProps): ReactNode => {
           ${lang !== "ko"? "bg-white  ":"bg-red-300"} `} 
           onClick={()=>{setLang((prev:"ko"|"en") => prev == "ko" ? "en" : "ko")}}>
       <div className={`rounded-full w-[30px] h-[30px] m-[2px] flex items-center justify-center absolute cursor-pointer transition-all duration-300
-          ${lang === "ko"? "bg-white text-red-300":"bg-red-300 text-white ml-10"} `}>
+          ${lang === "ko"? "pt-0.5 bg-white text-red-300 text-lg":"bg-red-300 text-white ml-10"} `}>
           {lang === "ko" ? '한' : 'en'}
       </div>
       <div className={`rounded-full w-[30px] h-[30px] m-[2px] flex items-center justify-center transition-all duration-300
-          ${lang !== "ko"? "bg-white text-red-300":"bg-red-300 text-white ml-10"} absolute`}>
+          ${lang !== "ko"? "pt-1 bg-white text-red-300":"text-lg pt-0.5 bg-red-300 text-white ml-10"} absolute`}>
           {lang === "ko" ? '영' : 'ko'}
       </div>
       </div>
