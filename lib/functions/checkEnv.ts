23b7b3dc116e32378a902b1708369d605e03143f
toothless8392 <hsp5235@naver.com>
2024-03-15 00:09:19 +0900

    add address

--- lib/functions/checkEnv.ts
+++ lib/functions/checkEnv.ts
@@ -2,7 +2,7 @@ export const checkEnvironment = () => {
     let base_url =
       process.env.NODE_ENV === "development"
         ? "http://localhost:3000"
-        : "https://example.com";
+        : "https://next-meet-web.vercel.app";
   
     return base_url;
   };
